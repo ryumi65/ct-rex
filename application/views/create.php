642eb72be7182e00eb22eaf8acfe29b4d84fae2e
rezaputraa <mrezaputraa@gmail.com>
2022-10-26 16:48:24 +0700

    Added create process

---
+++ application/views/create.php
@@ -0,0 +1,23 @@
+<?= validation_errors(); ?>
+<?= form_open('mahasiswa/create') ?>
+	NIM<br>
+	<input type="text" name="nim"><br><br>
+
+	Nama<br>
+	<input type="text" name="nama"><br><br>
+
+	Jenis Kelamin<br>
+	<input type="radio" name="jenis_kelamin" value="L">Laki-laki<br>
+	<input type="radio" name="jenis_kelamin" value="P">Perempuan<br><br>
+
+	Tempat Lahir<br>
+	<input type="text" name="tempat_lahir"><br><br>
+
+	Tanggal Lahir<br>
+	<input type="date" name="tanggal_lahir"><br><br>
+
+	Tahun Angkatan<br>
+	<input type="text" name="tahun_angkatan"><br><br>
+
+	<input type="submit" value="Simpan">
+</form>

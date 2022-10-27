2d43fce84d469b829e1202b5a4782cca65e455a8
- <renalsukma@gmail.com>
2022-10-27 10:37:51 +0700

    commit renal

---
+++ application/controllers/matkul.php
@@ -0,0 +1,55 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class matkul extends CI_Controller {
+
+    public function __construct()
+	{
+        parent::__construct();
+        
+		$this->load->model('model_matkul');
+	}
+	public function index()
+	{
+        $data['list'] = $this->model_matkul->get_matkul();
+
+		$this->load->view('matkul', $data);
+	}
+    public function create() {
+        $this->form_validation->set_rules('id_matkul','IDMATKUL','required');
+        $this->form_validation->set_rules('nama','NAMA','required');
+        $this->form_validation->set_rules('sks','SKS','required');
+        $this->form_validation->set_rules('status_matkul','STATUS','required');
+        $this->form_validation->set_rules('nik_dosen','NIK','required');
+
+        if (!$this->form_validation->run()){
+            $this->load->view('create');
+        } else{
+            $this->model_matkul->set_matkul();
+            redirect('matkul');
+        }
+    }
+    public function update($id_matkul)
+	{
+		$data['matkul'] = $this->model_matkul->get_matkul($id_matkul);
+
+        $this->form_validation->set_rules('id_matkul','IDMATKUL','required');
+        $this->form_validation->set_rules('nama','NAMA','required');
+        $this->form_validation->set_rules('sks','SKS','required');
+        $this->form_validation->set_rules('status_matkul','STATUS','required');
+        $this->form_validation->set_rules('nik_dosen','NIK','required');
+
+		if (!$this->form_validation->run()) {
+			$this->load->view('update', $data);
+		} else {
+			$this->model_matkul->update_matkul($id_matkul);
+			redirect('matkul');
+		}
+	}
+
+	public function delete($id_matkul) {
+		$this->db->delete('matkul', ['id_matkul' => $id_matkul]);
+		redirect('matkul');
+	}
+    
+}
---
+++ application/views/matkul.php
@@ -0,0 +1,25 @@
+<h1>Selamat datang di halaman Mata Kuliah!</h1>
+<a href="<?= site_url('matkul/create') ?>">Tambah</a><br><br>
+<table border=1>
+    <tr>
+        <td>id matkul</td>
+        <td>nama</td>
+        <td>sks</td>
+        <td>status matkul</td>
+        <td>level matkul</td>
+        <td>nik dosen</td>
+    </tr>
+
+    <?php foreach ($list as $matkul):?>
+        <tr>
+            <td><?=$matkul['id_matkul']?></td>
+            <td><?=$matkul['nama']?></td>
+            <td><?=$matkul['sks']?></td>
+            <td><?=$matkul['status_matkul']?></td>
+            <td><?=$matkul['level_matkul']?></td>
+            <td><?=$matkul['nik_dosen']?></td>
+            <td><a href="<?= site_url('matkul/update/' . $matkul['id_matkul']) ?>">Edit</a>
+				<a href="<?= site_url('matkul/delete/' . $matkul['id_matkul']) ?>">Hapus</a></td>
+        </tr>
+    <?php endforeach ?>
+    </table>

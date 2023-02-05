421f690bc331242daecb125ae6313c03989db834
rezaputraa <mrezaputraa@gmail.com>
2023-02-05 17:01:28 +0700

    Navigasi pada profil dosen

--- application/views/dosen/update.php
+++ application/views/dosen/update.php
@@ -1,11 +1,23 @@
     <div class="main-content position-relative bg-gray-100 max-height-vh-100 h-100">
         <div class="container-fluid pt-6 pt-xl-0">
 
+            <!-- Navigasi -->
+            <div class="d-flex d-inline mt-4 mb-3">
+                <a class="badge bg-primary cursor-pointer px-3 py-2" onclick="javascript:history.go(-1)"><i class="fa-solid fa-arrow-left"></i></a>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb bg-gray-100 my-0 py-0">
+                        <li class="breadcrumb-item"><a href="<?= site_url('dosen') ?>"><u>Home</u></a></li>
+                        <li class="breadcrumb-item"><a href="<?= site_url('dosen/profil') ?>"><u>Profil</u></a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Ubah</li>
+                    </ol>
+                </nav>
+            </div>
+
             <!-- Profil -->
             <div class="col-12 my-4">
                 <div class="card">
                     <div class="card-header p-3 pb-0">
-                        <h5 class="mb-0">Edit Profil</h5>
+                        <h5 class="mb-0">Ubah Profil</h5>
                     </div>
                     <div class="card-body p-3">
                         <?= validation_errors() ?>
